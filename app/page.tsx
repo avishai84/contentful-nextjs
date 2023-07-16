be3dfcf0ea97af38a589b1d9321afede3181bbe8
Avishai Taashur <avishai@avishai84.com>
2023-07-15 19:42:37 -0700

    new index src stracture

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,9 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './page.module.css'
-const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env
-import {QueryDataProps} from "./types"
-
-const query = `
- {
-    dogBreedsCollection(limit:100){
-      items{
-        dogBreedName
-        sys{
-          id
-        }
-      }
-    }
-  }
-`
-
-const Dogs = ({data}:QueryDataProps) => {
-
-  return(
-  <>{data.data.dogBreedsCollection.items.map((dog) => (
-    <div key={dog.sys.id} className={styles.description}>
-      <h1>{dog.dogBreedName}</h1>
-      <Link href={`/dog/${dog.sys.id}`}>{dog.dogBreedName}</Link>
-    </div>
-  ))}</>
-  )};
+import React from "react";
+import Dogs from "./pages/";
 
 export default async function Page() {
-  const data = await fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}/environments/master`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        query:query,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${CONTENTFUL_ACCESS_TOKEN}`
-      },
-    }
-  ).then((res) => res.json());
   
-  return <main><Dogs data={data} /></main>;
+  
+  return <main><Dogs /></main>;
 }
 
---
+++ app/pages/index.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../page.module.css'
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env
+import {QueryDataProps} from "../types"
+
+const query = `
+ {
+    dogBreedsCollection(limit:100){
+      items{
+        dogBreedName
+        sys{
+          id
+        }
+      }
+    }
+  }
+`
+
+const Dogs = ({data}:QueryDataProps) => {
+
+  return(
+  <>{data.data.dogBreedsCollection.items.map((dog) => (
+    <div key={dog.sys.id} className={styles.description}>
+      <h1>{dog.dogBreedName}</h1>
+      <Link href={`/dog/${dog.sys.id}`}>{dog.dogBreedName}</Link>
+    </div>
+  ))}</>
+  )};
+
+export default async function Page() {
+  const data = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}/environments/master`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query:query,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${CONTENTFUL_ACCESS_TOKEN}`
+      },
+    }
+  ).then((res) => res.json());
+  
+  return <main><Dogs data={data} /></main>;
+}
+
